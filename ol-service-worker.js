05609609589ceec70b4b643a8ac261813977713e
Jose Antonio Sepulveda Gil <jose.sepulveda@lottoland.com>
2018-08-08 13:02:03 +0200

    new manifest

--- ol-service-worker.js
+++ ol-service-worker.js
@@ -1,4 +1,4 @@
-self.addEventListener('active', event => {
+self.addEventListener('install', event => {
     console.log(new URL(location).searchParams.get('apiKey'));
 });
 importScripts("https://cdn.otherlevels.com/js-sdk/ol-service-worker.js?appKey=7e45c406f16be97a0fdba58d0c6a7222");
