38b5d0cf928cb50018126f79e91647aaaaf809ea
Jose Antonio Sepulveda Gil <jose.sepulveda@lottoland.com>
2018-08-09 12:00:23 +0200

    test

--- ol-service-worker.js
+++ ol-service-worker.js
@@ -1,1 +1,2 @@
-importScripts("https://cdn.otherlevels.com/js-sdk/ol-service-worker.js?appKey="+new URL(location).searchParams.get('pathToJson'))
+importScripts("https://cdn.otherlevels.com/js-sdk/ol-service-worker.js?appKey="+new URL(location).searchParams.get('appKey'));
+console.log(new URL(location).searchParams.get('appKey'));
